ab0fd6e69513b0b75601c5f0c44fd2f389e86ffa
Paul Yousefi <py16069@bris.ac.uk>
2019-06-11 10:02:37 +0100

    add training scripts

---
+++ ranger.r
@@ -0,0 +1,47 @@
+## ----load-------------------------------------------------------------
+load("dataset.rda")
+
+## ----load.joehanes -------------------------------------------------------------
+load("joehanes2016_st2_bonf.rda")
+
+## ----subset.meth -------------------------------------------------------------
+X <- meth[joehanes$probe.id, ]
+## transpose to make columns = methylation site variables,
+##					rows = subjects/observations
+X <- t(X) 
+
+## ----data.partition -------------------------------------------------------------
+library(caret)
+
+set.seed(138) # makes random processes reproducible
+Y <- samples$ever.smoke
+in.train <- createDataPartition(
+  y = samples$ever.smoke,
+  ## the outcome data are needed
+  p = .75,
+  ## The percentage of data in the
+  ## training set
+  list = FALSE
+)
+
+## ----data.subset -------------------------------------------------------------
+training <- samples[ in.train,]
+testing  <- samples[-in.train,]
+
+nrow(training)
+nrow(testing)
+
+system.time(rf.fit <- train(y = as.factor(training$ever.smoke), 
+				x = X[in.train,], 
+                method = "glmnet"))
+
+rf.fit
+
+a<-proc.time(); a;
+fit <- train(y = as.factor(training$ever.smoke), 
+				x = X[in.train,], 
+                method = "ranger")
+b<-proc.time()-a; b;
+(time <- paste0("Computation time = ", round(b[3]/60, 2)," minutes"))
+
+save(list = c("fit"), file = "ranger.rda")
